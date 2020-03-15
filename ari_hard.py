91771f0c7e556e8e7fcaaf043c8e66f9b2dd7652
Ari Tumma <ari.tumma@gmail.com>
2020-03-15 12:40:21 -0500

    fixed bad copy

--- ari_hard.py
+++ ari_hard.py
@@ -1,23 +1,30 @@
 import random
 
+## A coin has head and tails - lets call it dice :) 
 dice = ("heads","tails")
-val = random.choice(dice)
-
+x = 20
+## initialize 
 count = 0
+ctr = 0
 try_h = 1
-while count <= 1000000:
+
+while count <= 10000000:
 
     #print("count is: ",count)
     val = random.choice(dice)
- 
+    #look for heads apearing back to back 
     if val == "heads":
-       if try_h >= 20: 
-          print("YESS!!!!!!! it took: ", try_h , " trys")
-       try_h = 1 
-    
+       # if it happens more than X times, success! 
+       if try_h >= x: 
+          print("YESS!!!!!!! it took: ", ctr , " tosses to get " ,x, "heads  in a row")
+          try_h = 0 #reset after success
+          ctr = 0
+       try_h = try_h + 1  # keep counting back-to-back heads
+       
     else:
-        #print(val)
-        try_h = try_h + 1 
+        
+        try_h = 1 #reset if not heads
+        ctr = ctr + 1
     
     count = count + 1
     
